3c9122fefcaeb7e98a2c4bc3d4ae0bc461092f47
diftodi <diftodi@extole.com>
2022-10-11 17:53:28 +0300

    DemoApp uses the new mobile_bootstrap creative

---
+++ app/src/main/java/com/extole/androidsdk/DemoApplication.kt
@@ -0,0 +1,7 @@
+package com.extole.androidsdk
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DemoApplication : Application()
--- app/src/main/java/com/extole/androidsdk/MainActivity.kt
+++ app/src/main/java/com/extole/androidsdk/MainActivity.kt
@@ -1,57 +1,41 @@
 package com.extole.androidsdk
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.extole.android.sdk.Extole
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    lateinit var sdkActivityContext: Context
+
+    @Inject
     lateinit var extole: Extole
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sdkActivityContext = this
 
         GlobalScope.launch {
-            extole = ServiceLocator.getExtole(this@MainActivity)
-            val (zone, campaign) = extole.fetchZone("apply_for_card")
+            val (zone, campaign) = extole.fetchZone("cta_prefetch")
             runOnUiThread {
-
-                findViewById<Button>(R.id.native_share).setText(
-                    zone.get("menu.message").toString(), TextView.BufferType.NORMAL
+                findViewById<Button>(R.id.menu_item).setText(
+                    zone.get("cta.title").toString(), TextView.BufferType.NORMAL
                 )
 
-                findViewById<EditText>(R.id.email_message).setText(
-                    zone.get("sharing.email.message").toString(), TextView.BufferType.NORMAL
-                )
-
-                findViewById<Button>(R.id.email_share).setText(
-                    zone.get("sharing.email.subject").toString(), TextView.BufferType.NORMAL
-                )
-
-                findViewById<TextView>(R.id.copyright).setText(
-                    zone.get("sharing.facebook.description").toString(),
-                    TextView.BufferType.NORMAL
-                )
-
-                findViewById<Button>(R.id.native_share).setOnClickListener {
-                    val intent = Intent(this@MainActivity, WebViewActivity::class.java)
-                    startActivity(intent)
+                findViewById<Button>(R.id.menu_item).setOnClickListener {
+                    GlobalScope.launch {
+                        extole.sendEvent(zone.get("cta.touch_event").toString())
+                    }
                 }
             }
 
-            launch {
-                val (customMenu1, _) = campaign.fetchZone("custom_menu_1")
-                runOnUiThread {
-                    findViewById<Button>(R.id.menu_item).text =
-                        customMenu1.get("message").toString()
+            findViewById<Button>(R.id.deeplink).setOnClickListener {
+                GlobalScope.launch {
+                    extole.sendEvent("deeplink", mapOf("extole_key" to "extole_value"))
                 }
             }
         }
